44d466f10080122c87b7182013a493f474c40a4c
Suite <suite@SUITESB505-ST19.local>
2023-11-21 20:00:44 -0600

    first commit

---
+++ ProyectoMovil/ProyectoMovilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoMovilApp.swift
+//  ProyectoMovil
+//
+//  Created by Suite on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoMovilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
